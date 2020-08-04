aa943123692ec9c7e7f40c3aedc0756b3fd570a3
smile <smile@DESKTOP-UR26VSS>
2020-08-04 16:04:04 +0900

    add

---
+++ bootwildfly/src/main/java/bootwildfly/HelloWildFlyController.java
@@ -0,0 +1,19 @@
+package bootwildfly;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWildFlyController {
+
+
+    @RequestMapping("hello")
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
+    
+    @RequestMapping("welcome")
+    public String sayWelcome(){
+        return ("Hello World!!!!");
+    }
+}
